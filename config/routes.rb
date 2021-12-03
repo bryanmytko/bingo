8c76112d6872699f1a0523c76fe4e1cba20ad5b1
Bryan Mytko <bryanmytko@gmail.com>
2021-12-03 05:47:31 -0500

    Update card resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   devise_for :users
 
-  resources :cards, only: [:create, :show, :update, :destroy]
+  resources :cards
 
   get '/*path' => "cards#show", constraints: -> (req) { req.fullpath =~ /\/[a-zA-Z0-9]{10}/ }
 end
