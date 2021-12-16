678ebf44f5f9a5740c3e36684620608e6d44e92e
Bryan Mytko <bryanmytko@gmail.com>
2021-12-16 08:32:37 -0500

    Add entries validation

--- app/javascript/scripts/bingo.js
+++ app/javascript/scripts/bingo.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+  /* Bingo card marks */
   const items = document.querySelectorAll(".bingo-card__item");
 
   for(let i = 0; i < items.length; i++) {
@@ -7,8 +8,8 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
+  /* Copy to clipboard */
   const shareLinks = document.querySelectorAll(".share-url");
-  console.log(shareLinks)
 
   for(let i = 0; i < shareLinks.length; i++) {
     shareLinks[i].addEventListener('click', function(e) {
@@ -18,6 +19,30 @@ document.addEventListener("DOMContentLoaded", () => {
       e.preventDefault();
     });
   }
+
+  /* Entries validation */
+
+  const MIN = 24;
+  const entries = document.getElementById('card_entries');
+  const entries_label = document.getElementsByClassName('entries-validation')[0];
+  entries_label.innerHTML = `${entriesCount()} / ${MIN} Entries`;
+
+  function entriesCount(){
+    const btn = document.getElementsByClassName('card-create-submit')[0];
+    const value = entries.value;
+    const split = value.split('\n').join(',').split(',').filter(e => e.trim());
+    const length = split.length;
+
+    btn.disabled = (length >= MIN) ? false : true;
+    
+    return split.length;
+  }
+
+  function updateCountLabel() {
+    entries_label.innerHTML = `${entriesCount()} / ${MIN} Entries`;
+  }
+
+  entries.addEventListener('keyup', updateCountLabel);
 });
 
 
