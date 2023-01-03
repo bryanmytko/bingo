8dd65994387a13f4a9fd984411fc7a49b08c6cfb
Bryan Mytko <bryanmytko@gmail.com>
2023-01-03 15:16:51 -0800

    Add DELETE test

--- spec/requests/cards_spec.rb
+++ spec/requests/cards_spec.rb
@@ -4,15 +4,14 @@ describe "/cards", type: :request do
   let(:user) { User.create(email: "test@test.com", password: "abc123!") }
 
   describe "POST /create" do
-    before(:each) { sign_in user }
     let(:card) {{ title: "foo", entries: ["foo", "bar"] }}
 
     describe "when the user is logged in" do
-      it "creates a new card" do
+      before(:each) { sign_in user }
 
-        expect {
-          post cards_url, params: { card: card }
-        }.to change(Card, :count).by(1)
+      it "creates a new card" do
+        expect { post cards_url, params: { card: card }}
+          .to change(Card, :count).by(1)
       end
     end
 
@@ -23,5 +22,34 @@ describe "/cards", type: :request do
         expect(response).to have_http_status(302)
       end
     end
+  end
+
+  describe "DELETE /:id" do
+    describe "when the user is logged" do
+      before(:each) { sign_in user }
+
+      it "deletes the card" do
+        card = Card.create!(user: user)
+        expect {
+          delete "/cards/#{card.id}"
+        }.to change(Card, :count).by(-1)
+      end
+
+      it "redirects to the home screen" do
+        card = Card.create!(user: user)
+        response = delete "/cards/#{card.id}"
+        expect(response).to be(302)
+      end
+
+      describe "when the card does not exist" do
+        before(:each) { sign_in user }
+
+        it "redirects to the home screen" do
+          response = delete "/cards/1"
+          expect(response).to be(302)
+        end
+      end
+    end
+
   end
 end
