2c131c169793801296a37da1df14bc22529ea2cc
Bryan Mytko <bryanmytko@gmail.com>
2023-09-16 04:28:27 -0400

    Update turbo stream to update

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,8 +3,6 @@ class GamesController < ApplicationController
     @game = Game.find_or_create_by(user: current_user, card_id: params[:card])
     @game.update(status: :active) unless @game.started?
 
-    @game
-
     respond_to do |format|
       format.turbo_stream { render :create }
     end
