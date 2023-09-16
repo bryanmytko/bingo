71627d84dd2d0dbd45f906b2ed4233639f7e5986
Bryan Mytko <bryanmytko@gmail.com>
2023-09-16 04:20:59 -0400

    Add games#create

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :cards
+  resources :games, only: [:create]
 
   get '/print/*path' => "cards#print", constraints: -> (req) { req.fullpath =~ /\/[a-zA-Z0-9]{10}/ }
   get '/*path' => "cards#show", constraints: -> (req) { req.fullpath =~ /\/[a-zA-Z0-9]{10}/ }
