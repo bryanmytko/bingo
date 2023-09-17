e880faa3465aa91394f1918e9078ab9a96b3b33e
Bryan Mytko <bryanmytko@gmail.com>
2023-09-16 20:59:20 -0400

    Add buttons turbo stream render

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,15 @@
 class GamesController < ApplicationController
   def create
-    @game = Game.find_or_create_by(user: current_user, card_id: params[:card])
+    card_id = params[:card]
+    @game = Game.find_or_create_by(user: current_user, card_id: card_id)
     @game.update(status: :active) unless @game.started?
 
     respond_to do |format|
-      format.turbo_stream { render :create }
+      format.turbo_stream do
+         render turbo_stream: [
+          turbo_stream.update("buttons-#{card_id}", partial: "games/buttons", locals: { game: @game })
+        ]
+      end
     end
   end
 
