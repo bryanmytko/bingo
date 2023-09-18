b89ec61285ef7f50cab8f94724076a834fdf2973
Bryan Mytko <bryanmytko@gmail.com>
2023-09-18 02:29:44 -0400

    Add games#update for setting game status

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,16 +7,21 @@ class GamesController < ApplicationController
     respond_to do |format|
       format.turbo_stream do
          render turbo_stream: [
-          turbo_stream.update("buttons-#{card_id}", partial: "games/buttons", locals: { game: @game }),
+          turbo_stream.update("buttons-#{card_id}", partial: "games/buttons"),
           turbo_stream.update("game-dashboard", partial: "games/dashboard", locals: { game: @game })
         ]
       end
     end
   end
 
+  def update
+    game = Game.find(params[:game])
+    game.update(status: params[:status])
+  end
+
   private 
 
   def game_params
-    params.require(:game).permit(:card)
+    params.require(:game).permit(:card, :game, :status)
   end
 end
