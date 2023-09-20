c8a2463e9a9080ab814ac4f26ece29a7862a0dfc
Bryan Mytko <bryanmytko@gmail.com>
2023-09-20 18:13:54 -0400

    Display when user connects; remove old index

--- app/javascript/channels/games_channel.js
+++ app/javascript/channels/games_channel.js
@@ -1,27 +1,22 @@
 import consumer from "./consumer";
 
-console.log("a;klsj;lkajsdf");
-
 consumer.subscriptions.create(
   { channel: "GamesChannel" },
   {
     received(data) {
-      this.appendLine(data);
+      this.appendUser(data);
     },
 
-    appendLine(data) {
+    connected() {},
+
+    appendUser(data) {
       const html = this.createLine(data);
       const element = document.querySelector("[data-game-dashboard='users']");
       element.insertAdjacentHTML("beforeend", html);
     },
 
     createLine(data) {
-      return `
-      <article class="chat-line">
-        <span class="speaker">${data}</span>
-        <span class="body">${data["body"]}</span>
-      </article>
-    `;
+      return `<span class="speaker">${data.user.email}</span>`;
     },
   }
 );
