8cd51d2b679d0eab95c0ee91834ec56c3d86f1a8
Bryan Mytko <bryanmytko@gmail.com>
2023-09-20 18:13:26 -0400

    Import action cable and games channel

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
+import "@rails/actioncable";
+
 import "controllers";
+import "channels/games_channel";
+
 import "copy";
