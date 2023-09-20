e9fc93a32ae363e2df86a2336385868ebde2380a
Bryan Mytko <bryanmytko@gmail.com>
2023-09-20 18:01:55 -0400

    Pin actioncable & channels

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,7 +2,10 @@
 
 pin "application", preload: true
 pin "copy", preload: true
+pin "games_channel", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin_all_from "app/javascript/channels", under: "channels"
+pin "@rails/actioncable", to: "https://ga.jspm.io/npm:@rails/actioncable@7.0.8/app/assets/javascripts/actioncable.esm.js"
