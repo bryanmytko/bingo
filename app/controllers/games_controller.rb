185aa78cfdf053190bc7d83ac7221ae2f8123e91
Bryan Mytko <bryanmytko@gmail.com>
2023-09-23 18:18:22 -0400

    Add end game feature

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,12 @@
 class GamesController < ApplicationController
   def create
-    @game = Game.find_or_create_by(user: current_user, card: params[:card])
-    @game.update(status: :active) unless @game.started?
+    @game = Game.find_or_create_by(user: current_user, card_id: params[:card_id])
+    @game.update(status: :active)
 
     respond_to do |format|
       format.turbo_stream do
         render turbo_stream: [
-          turbo_stream.update("buttons-#{params[:card]}", partial: "games/buttons"),
+          turbo_stream.update("buttons-#{params[:card_id]}", partial: "games/buttons", locals: { card: @game.card }),
           turbo_stream.update("game-dashboard", partial: "games/dashboard", locals: { game: @game })
         ]
       end
@@ -14,13 +14,24 @@ class GamesController < ApplicationController
   end
 
   def update
-    game = Game.find(params[:game])
+    #TODO refactor
+    game = Game.find(params[:id])
     game.update(status: params[:status])
+    # game.connections.destroy_all
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: [
+          turbo_stream.update("buttons-#{game.card.id}", partial: "games/buttons", locals: { card: game.card }),
+          turbo_stream.update("game-dashboard", partial: "games/dashboard", locals: { game: game })
+        ]
+      end
+    end
   end
 
   private
 
   def game_params
-    params.require(:game).permit(:card, :game, :status)
+    params.require(:game).permit(:card_id, :game, :id, :status)
   end
 end
