457f73a48f0fec463719a1a2b7a5c5e23506967f
Bryan Mytko <bryanmytko@gmail.com>
2023-09-24 23:32:23 -0400

    Update respond_to calls

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,21 +3,21 @@ class GamesController < ApplicationController
     @game = Game.find_or_create_by(user: current_user, card_id: params[:card_id])
     @game.update(status: :active)
 
-    respond_to(&:turbo_stream)
+    respond_to(:turbo_stream)
   end
 
   def update
     @game = Game.find(params[:id])
     @game.update(params)
 
-    respond_to(&:turbo_stream)
+    respond_to(:turbo_stream)
   end
 
   def destroy
     @game = Game.find(params[:id])
     @game.complete!
 
-    respond_to(&:turbo_stream)
+    respond_to(:turbo_stream)
   end
 
   private
