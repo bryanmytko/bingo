34e944a6fc4cacbad8128ec0d25db24debf37a7f
Bryan Mytko <bryanmytko@gmail.com>
2023-09-24 22:02:37 -0400

    Add games#destroy, clean up turbo_stream renders

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,30 +3,21 @@ class GamesController < ApplicationController
     @game = Game.find_or_create_by(user: current_user, card_id: params[:card_id])
     @game.update(status: :active)
 
-    respond_to do |format|
-      format.turbo_stream do
-        render turbo_stream: [
-          turbo_stream.update("buttons-#{params[:card_id]}", partial: "games/buttons", locals: { card: @game.card }),
-          turbo_stream.update("game-dashboard", partial: "games/dashboard", locals: { game: @game })
-        ]
-      end
-    end
+    respond_to(&:turbo_stream)
   end
 
   def update
-    #TODO refactor
-    game = Game.find(params[:id])
-    game.update(status: params[:status])
-    # game.connections.destroy_all
+    @game = Game.find(params[:id])
+    @game.update(params)
 
-    respond_to do |format|
-      format.turbo_stream do
-        render turbo_stream: [
-          turbo_stream.update("buttons-#{game.card.id}", partial: "games/buttons", locals: { card: game.card }),
-          turbo_stream.update("game-dashboard", partial: "games/dashboard", locals: { game: game })
-        ]
-      end
-    end
+    respond_to(&:turbo_stream)
+  end
+
+  def destroy
+    @game = Game.find(params[:id])
+    @game.complete!
+
+    respond_to(&:turbo_stream)
   end
 
   private
